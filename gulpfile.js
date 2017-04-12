e05cc3ce7fad747b9f1e78b7e1a6bb5d43ef99fc
Chris Donovan <breakthestatic@gmail.com>
2017-04-12 15:10:34 -0700

    Removed un-needed lambda opts config

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,11 @@ const gulp = require('gulp');
 const lambda = require('gulp-awslambda');
 const zip = require('gulp-zip');
 const moment = require('moment');
-const opts = {
-	region: 'us-east-1'
-};
  
 gulp.task('default', () =>
     gulp.src(['./**', '!./dist/**'])
         .pipe(zip('Build_' + moment().format('YYYY-MM-DD-HH.mm.ss') + '.zip'))
-        .pipe(lambda('squeezeBox', opts))
+        .pipe(lambda('squeezeBox'))
         .pipe(gulp.dest('./dist'))
         
 );
